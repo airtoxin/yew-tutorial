84bcd29ed08ccf8e1ae5a4490a8916cab3d26b0b
airtoxin <airtoxin@icloud.com>
2019-04-09 06:50:35 +0900

    Add counter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
 
-struct Model { }
+struct Model {
+    count: i32,
+}
 
 enum Msg {
     DoIt,
@@ -13,13 +15,16 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Model { }
+        Model {
+            count: 0
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::DoIt => {
                 // Update your model on events
+                self.count = self.count + 1;
                 true
             }
         }
@@ -30,7 +35,7 @@ impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
         html! {
             // Render your model here
-            <button onclick=|_| Msg::DoIt,>{ "Click me!" }</button>
+            <button onclick=|_| Msg::DoIt,>{self.count}</button>
         }
     }
 }
